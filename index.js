4f64ad05eb86c58839962996534dd44aff59b211
Inga Zhelnova <detal.07@mail.ru>
2024-02-21 21:19:59 +0300

    created a new item

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+{
+  const item_1 = "Assol";
+  const count = 100;
+  const category = "A home dress";
+  const price = 15;
+
+	console.log(item_1);
+  console.log(`Total price is: ${count * price}`);
+}
+
+{
+  const item_2 = "T-short";
+  const count = 23;
+  const category = "Sport T-shorts";
+  const price = 110;
+
+  console.log(item_2);
+  console.log(`Total price is: ${count * price}`);
+}
