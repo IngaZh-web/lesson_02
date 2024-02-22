ef29b9e7d8b909939c0d9338ea71659db8309aca
Inga Zhelnova <detal.07@mail.ru>
2024-02-22 13:20:49 +0300

    homework module 1 task 3

---
+++ einstein.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const myWeight = 65.5;
+const speedLight = 3e8;
+
+const e = myWeight * (speedLight * speedLight);
+
+console.log(e);
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-"use strict";
+// "use strict";
 
-{
-  const item_1 = "Assol";
-  const count = 100;
-  const category = "A home dress";
-  const price = 15;
+// {
+//   const item_1 = "Assol";
+//   const count = 100;
+//   const category = "A home dress";
+//   const price = 15;
 
-	console.log(item_1);
-  console.log(`Total price is: ${count * price}`);
-}
+// 	console.log(item_1);
+//   console.log(`Total price is: ${count * price}`);
+// }
 
-{
-  const item_2 = "T-short";
-  const count = 23;
-  const category = "Sport T-shorts";
-  const price = 110;
+// {
+//   const item_2 = "T-short";
+//   const count = 23;
+//   const category = "Sport T-short";
+//   const price = 110;
 
-  console.log(item_2);
-  console.log(`Total price is: ${count * price}`);
-}
+//   console.log(item_2);
+//   console.log(`Total price is: ${count * price}`);
+// }
+
+// {
+// 	const item_3 = prompt("Блуза");
+// 	const count_3 = Number(prompt(77));
+// 	const category_3 = prompt("Блуза женская");
+// 	const price_3 = Number(prompt(110));
+
+// 	console.log(typeof item_3);
+// 	console.log(typeof count_3);
+// 	console.log(typeof category_3);
+// 	console.log(typeof price_3);
+
+// 	console.log(`На складе ${count_3} единиц "${category_3}", на сумму: ${count_3 * price_3} руб.`)
+// }
